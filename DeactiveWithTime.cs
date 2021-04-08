b49cba8f8caa2cf58ddf64564556d39fa022371a
toai.siu <38150154+SiuToai@users.noreply.github.com>
2021-04-08 14:43:11 +0700

    Add files via upload

---
+++ DeactiveWithTime.cs
@@ -0,0 +1,28 @@
+﻿
+using UnityEngine;
+
+public class DeactiveWithTime : MonoBehaviour
+{
+
+    [SerializeField]
+    private float time = 1.1f;
+
+    private float currentTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentTime = time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentTime -= Time.deltaTime;
+        if(currentTime <= 0)
+        {
+            currentTime = time;
+            gameObject.SetActive(false);
+        }
+    }
+}
